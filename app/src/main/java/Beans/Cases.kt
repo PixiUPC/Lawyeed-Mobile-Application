04646801252ccf3cc701ef85b8f59fa1fa18d918
djalmadioses <54141599+djalmadioses@users.noreply.github.com>
2022-10-26 09:59:16 -0500

    feat: add recycler view (adapter and viewHolder) and cardview

---
+++ app/src/main/java/Beans/Cases.kt
@@ -0,0 +1,15 @@
+package Beans
+
+class Cases {
+    constructor(id: Int, title: String, description: String, status: String) {
+        this.id = id
+        this.title = title
+        this.description = description
+        this.status = status
+    }
+
+    var id:Int
+    var title:String
+    var description:String
+    var status:String
+}
